3b1077c0fa10ca1d1991a2acfd69b81b1ffad3fb
Kleak <kevin@lefty.io>
2017-06-15 09:40:48 +0200

    add must call super on container

--- lib/src/directives/container.dart
+++ lib/src/directives/container.dart
@@ -4,6 +4,7 @@ import 'package:kl/src/align_item.dart';
 import 'package:kl/src/directives/box.dart';
 import 'package:kl/src/display.dart';
 import 'package:kl/src/flex_wrap.dart';
+import 'package:meta/meta.dart';
 
 @Directive(
   selector: 'kl-container, [kl-container]',
@@ -42,6 +43,7 @@ class KlContainer extends KlBox implements OnInit {
   KlContainer(ElementRef elementRef) : super(elementRef);
 
   @override
+  @mustCallSuper
   void ngOnInit() {
     display = KlDisplay.flex;
     direction = 'column';
