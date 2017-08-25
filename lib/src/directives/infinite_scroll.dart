796ce2236efbf17a5c496c8e88112c8b52118d13
Kleak <segaud.kevin@gmail.com>
2017-08-25 11:28:12 +0200

    add infinte scroll

---
+++ lib/src/directives/infinite_scroll.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+import 'dart:html';
+
+import 'package:angular2/angular2.dart';
+import 'package:kl/src/directives/container.dart';
+
+@Directive(
+  selector: 'kl-infinite-scroll',
+  inputs: const [
+    'alignItems',
+    'flexWrap',
+    'justifyContent',
+    'direction',
+    'constraint',
+    'decoration',
+    'padding',
+    'margin',
+  ],
+)
+class KlInfinteScroll extends KlContainer implements OnInit, OnDestroy {
+  final StreamController<Null> _fireScrollEnd = new StreamController();
+  StreamSubscription<Event> _scrollSubscription;
+
+  @Input('threshold')
+  int threshold;
+
+  @Input('fireEvent')
+  bool fireEvent = true;
+
+  KlInfinteScroll(ElementRef elementRef) : super(elementRef);
+
+  @override
+  void ngOnInit() {
+    super.ngOnInit();
+    _scrollSubscription = window.onScroll.listen(_onScroll);
+  }
+
+  @override
+  void ngOnDestroy() {
+    _fireScrollEnd?.close();
+    _scrollSubscription?.cancel();
+  }
+
+  void _onScroll(Event event) {
+    if (fireEvent) {
+      final alreadyScrolled =
+          ((window.scrollY + document.body.clientHeight) * 100) /
+              document.documentElement.scrollHeight;
+      if (alreadyScrolled > threshold) {
+        _fireScrollEnd.add(null);
+      }
+    }
+  }
+}
