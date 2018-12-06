ebf454f3a4caad3f9d9eeaf8b57762e02668cbef
Morgan McKenzie <rmtmckenzie@gmail.com>
2018-12-06 12:21:17 -0800

    Make KlFlexible flex parameter 'flex', add other flex options explicitly

--- lib/src/directives/flexible.dart
+++ lib/src/directives/flexible.dart
@@ -9,10 +9,35 @@ import 'package:kl/src/element.dart';
   selector: 'kl-flexible, [kl-flexible]',
 )
 class KlFlexible extends KlElement {
+  // maintain backwards compatibility
   @Input('kl-flexible')
+  set klFlexible(String value) => flex = value;
+
+  @Input()
   set flex(String value) {
     if (value != null) {
       element.style.flex = value;
+    }
+  }
+
+  @Input()
+  set flexBasis(String value) {
+    if (value != null) {
+      element.style.flexBasis = value;
+    }
+  }
+
+  @Input()
+  set flexShrink(String value) {
+    if (value != null) {
+      element.style.flexShrink = value;
+    }
+  }
+
+  @Input()
+  set flexGrow(String value) {
+    if (value != null) {
+      element.style.flexGrow = value;
     }
   }
 
